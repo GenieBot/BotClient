16d96265976460c3788aac74322316ed0be6172e
SpongyBacon <spongybacon@hotmail.co.uk>
2016-03-24 23:40:19 +0000

    User join message

--- src/main/java/io/sponges/bot/client/Main.java
+++ src/main/java/io/sponges/bot/client/Main.java
@@ -2,6 +2,7 @@ package io.sponges.bot.client;
 
 import io.sponges.bot.client.event.events.CommandResponseEvent;
 import io.sponges.bot.client.protocol.msg.ChatMessage;
+import io.sponges.bot.client.protocol.msg.UserJoinMessage;
 
 import java.util.Scanner;
 
@@ -9,24 +10,26 @@ public class Main {
 
     public Main() {
         Bot bot = new Bot("cli", "-", "localhost", 9574);
-
         bot.getEventBus().register(CommandResponseEvent.class, (event) -> {
             System.out.println(event.getMessage());
         });
-
         new Thread(() -> {
             Scanner scanner = new Scanner(System.in);
             String input;
             while ((input = scanner.nextLine()) != null) {
-                ChatMessage chatMessage = new ChatMessage(bot, "cli", "cli", true, "cli", null, null, false, true,
+                ChatMessage chatMessage = new ChatMessage(bot, "cli", "cli", false, "cli", null, null, false, true,
                         System.currentTimeMillis(), input);
                 bot.getClient().sendMessage(chatMessage.toString());
-
                 if (input.equalsIgnoreCase("-stop")) break;
+                if (input.equalsIgnoreCase("userjoin")) {
+                    UserJoinMessage userJoinMessage = new UserJoinMessage(bot, "cli", "cli", "addedUserId",
+                            "addedUsername", "addedDisplayName", false, false, "initiatorId", "initiatorUsername",
+                            "initiatorDisplayName", true, false);
+                    bot.getClient().sendMessage(userJoinMessage.toString());
+                }
             }
             scanner.close();
         }).start();
-
         bot.start();
     }
 
