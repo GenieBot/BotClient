03615d4b954ef4ec7f7036d68c209007cb2f1f85
SpongyBacon <spongybacon@hotmail.co.uk>
2016-09-17 23:10:29 +0100

    Update testing class

--- src/main/java/io/sponges/bot/client/Main.java
+++ src/main/java/io/sponges/bot/client/Main.java
@@ -6,7 +6,6 @@ import io.sponges.bot.client.event.events.StopEvent;
 import io.sponges.bot.client.event.framework.EventBus;
 import io.sponges.bot.client.protocol.msg.ChatMessage;
 
-import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -15,53 +14,21 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 class Main {
 
-    private static final String LOGGER_FORMAT = "[%s - %s] %s\r\n";
     private static final String DUMMY_NETWORK = "cli";
     private static final String DUMMY_CHANNEL = "cli";
     private static final String DUMMY_USER = "cli";
 
-    private static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm:ss");
+    private final Bot bot;
 
-    public static void main(String[] args) {
+    private Main() {
         Logger logger = Bot.getLogger();
-        logger.setDebug(true);
+        logger.setDebug(false);
         logger.log(Logger.Type.INFO, "Starting...");
         long startTime = System.currentTimeMillis();
-        Bot bot = new Bot("cli", "-", "localhost", 9574);
+        this.bot = new Bot("cli", "-", "localhost", 9574);
         EventBus eventBus = bot.getEventBus();
-        eventBus.register(CommandResponseEvent.class, event -> {
-            logger.log("CMD", event.getMessage());
-        });
-        eventBus.register(ResourceRequestEvent.class, event -> {
-            Map<String, String> parameters = new HashMap<>();
-            switch (event.getType()) {
-                case NETWORK:
-                    if (!event.getNetworkId().equals(DUMMY_NETWORK)) {
-                        event.replyInvalid(bot);
-                        return;
-                    }
-                    parameters.put("name", "Dummy CLI network");
-                    break;
-                case CHANNEL:
-                    if (!event.getChannelId().equals(DUMMY_CHANNEL)) {
-                        event.replyInvalid(bot);
-                        return;
-                    }
-                    parameters.put("id", DUMMY_CHANNEL);
-                    parameters.put("private", String.valueOf(false));
-                    break;
-                case USER:
-                    if (!event.getUserId().equals(DUMMY_USER)) {
-                        event.replyInvalid(bot);
-                        return;
-                    }
-                    parameters.put("id", DUMMY_USER);
-                    parameters.put("admin", String.valueOf(true));
-                    parameters.put("op", String.valueOf(true));
-                    break;
-            }
-            event.reply(bot, parameters);
-        });
+        eventBus.register(CommandResponseEvent.class, this::onCommandResponse);
+        eventBus.register(ResourceRequestEvent.class, this::onResourceRequest);
         AtomicBoolean running = new AtomicBoolean(true);
         eventBus.register(StopEvent.class, event -> {
             logger.log(Logger.Type.DEBUG, "Got stop event lol");
@@ -84,6 +51,45 @@ class Main {
         bot.start();
         long diff = System.currentTimeMillis() - startTime;
         logger.log(Logger.Type.INFO, "Started! Took " + diff + "ms.");
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    private void onCommandResponse(CommandResponseEvent event) {
+        Bot.getLogger().log("CMD", event.getMessage());
+    }
+
+    private void onResourceRequest(ResourceRequestEvent event) {
+        Map<String, String> parameters = new HashMap<>();
+        switch (event.getType()) {
+            case NETWORK:
+                if (!event.getNetworkId().equals(DUMMY_NETWORK)) {
+                    event.replyInvalid(bot);
+                    return;
+                }
+                parameters.put("name", "Dummy CLI network");
+                break;
+            case CHANNEL:
+                if (!event.getChannelId().equals(DUMMY_CHANNEL)) {
+                    event.replyInvalid(bot);
+                    return;
+                }
+                parameters.put("id", DUMMY_CHANNEL);
+                parameters.put("private", String.valueOf(false));
+                break;
+            case USER:
+                if (!event.getUserId().equals(DUMMY_USER)) {
+                    event.replyInvalid(bot);
+                    return;
+                }
+                parameters.put("id", DUMMY_USER);
+                parameters.put("admin", String.valueOf(true));
+                parameters.put("op", String.valueOf(true));
+                break;
+        }
+        event.reply(bot, parameters);
     }
 
 }
